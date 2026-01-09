7d462f9d50479e487c692964104dec845f108850
Michele Di Gennaro <digennaromichele99@gmail.com>
2026-01-09 22:33:07 +0100

    FIX CRITICO: Aggiunto AuthorizesRequests al Controller base
    Il Controller.php base era vuoto (Laravel 11 default).
    Aggiunto:
    - use AuthorizesRequests: abilita \->authorize() per le policy
    - use ValidatesRequests: abilita \->validate()
    - extends BaseController: eredita da Illuminate Controller
    
    Questo fix risolve l'errore 500 'Call to undefined method authorize()'

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -15,16 +15,22 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
 /**
  * Controller base per tutti i controller dell'applicazione
  * 
  * Questo controller astratto fornisce funzionalità comuni
- * per tutti i controller dell'applicazione C2MS.
+ * per tutti i controller dell'applicazione SUGECO:
+ * - AuthorizesRequests: permette l'uso di $this->authorize()
+ * - ValidatesRequests: permette l'uso di $this->validate()
  * 
  * @package App\Http\Controllers
- * @version 1.0
+ * @version 1.1
  */
-abstract class Controller
+abstract class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, ValidatesRequests;
 }
